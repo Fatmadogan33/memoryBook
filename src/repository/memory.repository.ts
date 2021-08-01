8e95cac84fc36784a5a3d2b4812e8f08a7595ff3
Fatmadogan33 <fatoss.dogn@gmail.com>
2021-08-01 22:41:14 +0300

    upload

--- src/repository/memory.repository.ts
+++ src/repository/memory.repository.ts
@@ -5,58 +5,73 @@ import UpdateMemory from "../interface/update_memory";
 import MemoryDb from "../interface/memory";
 import Memory from "@entities/Memory";
 
-class MemoryBook{
+class MemoryBook {
 
-    memoryDB : typeof KnexDB;
-    constructor(){
+    memoryDB: typeof KnexDB;
+    constructor() {
         this.memoryDB = KnexDB;
     }
 
-    async getAllMemories(): Promise<Memory[]> {
+    async getAllMemories(): Promise<MemoryDb[]> {
         return new Promise((resolve, reject) => {
-          this.memoryDB.db
-            .select("id", "memory", "date", "location","title", )
-            .from("memories")
-            .then((result) => {
-                resolve(result[0]);
-            })
-            .catch((error) => {
-                reject(new DatabaseError(error));
-            })     
-})
-}
-
-
-async addMemory(id: number, memory : string) : Promise<MemoryDb>{
-    return new Promise(async(resolve, reject) =>{
-        this.memoryDB.db("memory")
-            .insert(memory)
-                .returning("*")
-                    .then((result) => {
-                        resolve(result[0]);
-                    })
-                    .catch((error) =>{
-                        reject(new DatabaseError(error));
-                    })
-    });
-}
-
-    async updateMemory(memory : UpdateMemory) : Promise<MemoryDb>{
-        return new Promise(async (resolve, reject) => {
-            const  updatedMemory = memory;
-            this.memoryDB.db("memory")
-                .where("memory.memoryid", memory.id)
-                    .update(updatedMemory, ["memory_id", "title", "memory", "date","location"])
-                        .then((result) => {
-                            resolve(result[0]);
-                        })
-                        .catch((error) =>{
-                            reject(new DatabaseError())
-                        })
+            this.memoryDB.db
+                .select("id", "memory", "date", "location", "title",)
+                .from("memories")
+                .then((result) => {
+                    resolve(result[0]);
+                })
+                .catch((error) => {
+                    reject(new DatabaseError(error));
+                })
         })
     }
 
-    async deleteMemory(id : number) : Promise<Boolean> {
+    async getMemory(id: number): Promise<MemoryDb> {
+        return new Promise((resolve, reject) => {
+            this.memoryDB.db
+                .select("id", "memory", "date", "location", "title",)
+                .from("memories")
+                .where("memoryid", id)
+                .then((result) => {
+                    resolve(result[0]);
+                })
+                .catch((error) => {
+                    reject(new DatabaseError(error));
+                })
+        })
+    }
+
+
+    async addMemory(memory: MemoryDb): Promise<MemoryDb> {
+        return new Promise(async (resolve, reject) => {
+            this.memoryDB.db("memory")
+                .insert(memory)
+                .returning("*")
+                .then((result) => {
+                    resolve(result[0]);
+                })
+                .catch((error) => {
+                    reject(new DatabaseError(error));
+                })
+        });
+    }
+
+    async updateMemory(memory: MemoryDb): Promise<MemoryDb> {
+        return new Promise(async (resolve, reject) => {
+            const updatedMemory = memory;
+            this.memoryDB.db("memory")
+                .where("memory.memoryid", memory.id)
+                .update(updatedMemory, ["memory_id", "title", "memory", "date", "location"])
+                .then((result) => {
+                    resolve(result[0]);
+                })
+                .catch((error) => {
+                    reject(new DatabaseError())
+                })
+        })
+    }
+
+    async deleteMemory(id: number): Promise<Boolean> {
         return new Promise(async (resolve, reject) => {
             this.memoryDB.db("memory").where("memory.memoryid", id).del()
                 .then(() => {
